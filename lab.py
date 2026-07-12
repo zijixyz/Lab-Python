49828e6148dbd46cf8e5e48a27542b5398fb63e4
ziji <newbaong3@gmail.com>
2026-07-12 17:06:23 +0700

    belajar string

--- lab.py
+++ lab.py
@@ -1,8 +1,10 @@
-txt = "How are you doing today?"
-
-print("todays" in txt)
-
-if "todays" in txt:
-    print("Yes, 'today' is present in the string.")
-else:
-    print("No, 'today' is not present in the string.")
+# Create the variable
+txt = "Hello, World!"
+# Print characters from index 2 to 5
+print(txt[3:5])
+# Print in upper case
+print(txt.upper())
+# Create the name variable
+name = "Python"
+# Print using an f-string
+print(f"I love {name}")
