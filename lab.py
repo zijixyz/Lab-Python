0510ab6217b0cd239892e1c7e70038aa05de1189
Ziji <newbaong3@gmail.com>
2026-07-12 13:16:43 +0700

    belajar python

--- lab.py
+++ lab.py
@@ -1,3 +1,8 @@
-# pr(x)
-print("percobaan")
-Print("percobaan")
+txt = "How are you doing today?"
+
+print("todays" in txt)
+
+if "todays" in txt:
+    print("Yes, 'today' is present in the string.")
+else:
+    print("No, 'today' is not present in the string.")
