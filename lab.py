30d5037be90b7b194caa9c06e3ff4c53847534b1
ziji <newbaong3@gmail.com>
2026-07-18 16:22:21 +0700

    study different + and extend
    Added examples demonstrating list behavior and reference in Python.

--- lab.py
+++ lab.py
@@ -1,16 +1,17 @@
-# Study lists, tuples, sets, and dictionaries in Python
+a = ["apple", "banana"]
+b = a
 
-list = ["banana", "apple", "orange", "grape", "kiwi", "banana"]
-tuple = ("banana", "apple", "orange", "grape", "kiwi", "banana")
-set = {"banana", "apple", "orange", "grape", "kiwi", "banana"}
-dictionary = {"fruit1": "banana", "fruit2": "apple", "fruit3": "orange", "fruit4": "grape", "fruit5": "kiwi", "fruit6": "banana"}
+print(a)  # ['apple', 'banana']
+print(b)  # ['apple', 'banana']
+print("=============")
 
-print(list)
-print(tuple)
-print(set)
-print(dictionary)
-print("================================")
-print(len(list))
-print(len(tuple))
-print(len(set))
-print(len(dictionary))
+a.extend(["cherry"])  # b ikut berubah
+
+print(a)  # ['apple', 'banana', 'cherry']
+print(b)  # ['apple', 'banana', 'cherry']
+print("=============")
+
+a = a + ["durian"]    # a jadi list baru, b tetap lama
+
+print(a)  # ['apple', 'banana', 'cherry', 'durian']
+print(b)  # ['apple', 'banana', 'cherry'] (tidak berubah)
