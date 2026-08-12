73dd3d05f8ceb5429a49be4c62e02584ea52bae9
ziji <newbaong3@gmail.com>
2026-08-12 20:37:37 +0700

    Repeat the knowladge

--- lab.py
+++ lab.py
@@ -1,19 +1,3 @@
-a = ["apple", "banana"]
-b = a
-
-print(a)
-print(b)
-
-print("=============")
-
-a.extend(["cherry"])
-
-print(a)
-print(b)
-
-print("=============")
-
-a = a + ["durian"]
-
-print(a)
-print(b)
+"""
+"JUST REPEAT THE  KNOWLADGE"
+"""
