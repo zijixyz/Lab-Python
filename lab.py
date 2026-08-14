4efea62c36a7ebf37afc3638868f931513a6b7ed
ziji <newbaong3@gmail.com>
2026-08-14 16:13:38 +0700

    Membuat list sederhana

--- lab.py
+++ lab.py
@@ -1,3 +1,9 @@
-"""
-"JUST REPEAT THE  KNOWLADGE"
-"""
+tugas = []
+
+while input("Apakah Anda ingin menambahkan tugas? (Iya/Tidak): ").lower() == "iya":
+    tugas_baru = input("Masukkan tugas baru: ")
+    tugas.append(tugas_baru)
+
+    print(f"\nTugas {tugas_baru} berhasil ditambahkan.\n")
+
+print(f"\nDaftar Tugas: {tugas}")
