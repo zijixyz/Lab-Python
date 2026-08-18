5d645df6d0691f1a09c69f22b3af912603e485eb
ziji <newbaong3@gmail.com>
2026-08-18 17:57:34 +0700

    belajar tuple

--- lab.py
+++ lab.py
@@ -1,39 +1,30 @@
-# x = (1, 2, 3)
-# print(x)  # Output: (1, 2, 3)
-# print(type(x))  # Output: <class 'tuple'>
-# x = list(x)
-# print(x)  # Output: [1, 2, 3]
-# print(type(x))  # Output: <class 'list'>
-# x[0] = 10
-# print(x)  # Output: [10, 2, 3]
-# print(type(x))  # Output: <class 'list'>
-# x = tuple(x)
-# print(x)  # Output: (10, 2, 3)
-# print(type(x))  # Output: <class 'tuple'>
+# thistuple = ("apple", "banana", "cherry")
+# i = 0
 
-# =====================================================
+# while i < len(thistuple):
+#     print(thistuple[i])
+#     i += 1
 
-# thistuple = (1, 2, 3, 4, 5)
-# print(thistuple)  # Output: (1, 2, 3, 4, 5)
-# newtuple = (6, 7, 8, 9, 10)
-# print(newtuple)  # Output: (6, 7, 8, 9, 10)
-# thistuple += newtuple
-# print(thistuple)  # Output: (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) 
+# tuple1 = ("apple", "banana", "cherry")
+# tuple2 = ("orange", "kiwi", "melon", "mango")
+# tuple3 = tuple1 * 2 + tuple2
+# print(tuple3)
 
-# ================================================================
+# # Create the tuple
+# fruits = ("apple","banana","cherry")
+# # Print the second item
+# print(fruits[1])
+# # Print the number of items
+# print(len(fruits))
+# # Unpack the tuple
+# ("a","b","c") = fruits
+# # Print b
+# print(fruits[1])
 
-# thistuple = (1, 2, 3, 4, 5)
-# del thistuple
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
 
-# if thistuple:
-#     print(thistuple)
-# else:
-#     print("The tuple has been deleted.")  # Output: The tuple has been deleted.
+# (green, *yellow, red) = fruits
 
-# ====================================================================================
-
-tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-(apple, *banana, cherry) = tuple
-print(apple)  # Output: The first fruit is: apple
-print(banana)  # Output: The second fruit is: banana
-print(cherry)  # Output: The third fruit is: cherry
+# print(green)
+# print(yellow)
+# print(red)
